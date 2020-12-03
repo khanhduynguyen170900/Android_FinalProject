635ee84f08b56c08643e1e394308ead5cfbf9f57
Duy Khanh <khanhduynguyen170900@gmail.com>
2020-12-03 11:06:02 +0700

    Create project

---
+++ app/src/main/java/tdtu/projectfinal/toeic_listening_practice/SecondActivity.java
@@ -0,0 +1,14 @@
+package tdtu.projectfinal.toeic_listening_practice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
